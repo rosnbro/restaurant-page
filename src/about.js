fca160e28a0f22d2394d47caa4c66fc46f431096
rosnbro <benro95@gmail.com>
2022-07-02 13:22:38 -0400

    added contact page style, refined style for other pages

--- src/about.js
+++ src/about.js
@@ -6,6 +6,7 @@ function renderAbout() {
 
     aboutContent.classList.add('about');
     card.classList.add('card');
+    cardText.classList.add('aboutText');
 
     cardTitle.textContent = 'About Us';
     cardText.textContent = 'Here at Pi Place, we believe in sharing delicious homemade pies for an affordable & thematically appropriate price. Our pies are baked from scratch using the highest quality ingredients, and available to you at our convenient location in the Blacksburg Farmers Market. \nPlease come by our table and grab a slice!';
--- src/contact.js
+++ src/contact.js
@@ -3,33 +3,35 @@ function renderContact() {
     const card = document.createElement('div');
     const cardTitle = document.createElement('h2');
     const cardContent = document.createElement('div');
-
     const comSection = document.createElement('div');
+    const phoneLabel = document.createElement('p');
     const phoneNum = document.createElement('a');
+    const emailLabel = document.createElement('p');
     const email = document.createElement('a');
-
     const addySection = document.createElement('div');
     const addressContact = document.createElement('address');
-    const hoursContact = document.createElement('div');
+    const hoursContact = document.createElement('p');
     
     contactContent.classList.add('contact');
     card.classList.add('card');
     cardContent.classList.add('contactContent');
 
     cardTitle.textContent = 'Contact';
-    phoneNum.href = 'tel:+5405553141';
-    phoneNum.textContent = 'Phone: (540) 555 3141';
+    phoneLabel.textContent = 'Phone:'
+    phoneNum.href = 'tel:+13145551592';
+    phoneNum.textContent = '(314) 555-1592';
+    emailLabel.textContent = 'Email:';
     email.href = 'mailto:piplace@example.com';
-    email.textContent = 'Email: piplace@example.com';
+    email.textContent = 'piplace@example.com';
     hoursContact.innerHTML = '<b>Hours</b><br>Wed: 2PM - 6PM<br>Sat: 8AM - 2PM';
     addressContact.innerHTML = 'Blacksburg Farmers Market<br>108 W Roanoke St.<br>Blacksburg, VA 24060';
 
+    comSection.appendChild(phoneLabel);
     comSection.appendChild(phoneNum);
+    comSection.appendChild(emailLabel);
     comSection.appendChild(email);
-
     addySection.appendChild(addressContact);
     addySection.appendChild(hoursContact);
-
     cardContent.appendChild(comSection);
     cardContent.appendChild(addySection);
     card.appendChild(cardTitle);
